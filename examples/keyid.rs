b0bbd2e1608ca3ce8c2919c2013814c913234978
AndyJado <101876416+AndyJado@users.noreply.github.com>
2022-10-08 16:22:41 +0800

    clean

--- examples/keyid.rs
+++
@@ -1,8 +0,0 @@
-use duckbubble::parts::KeyId;
-
-fn main() {
-    let mut ids = KeyId::new();
-    loop {
-        dbg!(ids.next());
-    }
-}
--- examples/toml-read.rs
+++
@@ -1,9 +0,0 @@
-use std::io;
-
-use duckbubble::parts::Config;
-
-fn main() -> io::Result<()> {
-    let cfg = Config::read("dry.toml");
-    dbg!(cfg);
-    Ok(())
-}
